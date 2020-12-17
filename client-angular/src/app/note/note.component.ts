573b3ff1f6f3d7880c1f0fdd5b93af9426ded60d
joey <joeyegan92@gmail.com>
2020-12-16 19:09:52 -0600

    commit

--- client-angular/src/app/note/note.component.ts
+++ client-angular/src/app/note/note.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from "../task.service";
+import { Note } from "../models/Note";
 
 @Component({
   selector: 'app-note',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
+  constructor(private taskService: TaskService) {}
+  newNote: Note = new Note();
+  Note: Note[] = [];
 
-  constructor() { }
+  getNote() {
+    this.taskService.getNote().subscribe(tasks => (this.note = Note));
+  }
 
+  addNote() {
+    this.taskService.addNote(this.newNote).subscribe(result => {
+      this.getNote();
+      this.newNote = new Note();
+    });
+  }
+  
   ngOnInit(): void {
+    This.getNote();
   }
 
 }
--- client-angular/src/app/task.service.ts
+++ client-angular/src/app/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Task } from "./models/task";
 import { Observable } from "rxjs";
+import { Note } from "../models/Note";
 
 @Injectable({
   providedIn: "root"
@@ -18,5 +19,21 @@ export class TaskService {
 
   addTask(task): Observable<Task> {
     return this.http.post<Task>(this.apiUrl, task);
+
+    
   }
+  export class TaskService {
+    // Java Spring Boot uses port 8080
+    apiUrl: string = "http://localhost:8080/note";
+  
+    constructor(private http: HttpClient) {}
+
+    getNote(): Observable<Note[]> {
+      return this.http.get<Note[]>(this.apiUrl);
+    }
+  
+    addNote(Note): Observable<Note> {
+      return this.http.post<Note>(this.apiUrl, Note);
+  
+
 }
